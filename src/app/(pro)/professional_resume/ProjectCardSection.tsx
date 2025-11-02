2abc1a4163345f74dc12ef80a336d8a2fd6cb9be
Daniel Muljono <daniel.muljono@yahoo.co.id>
2025-11-03 00:02:13 +0700

    Card  Angled

--- src/app/(pro)/professional_resume/ProjectCardSection.tsx
+++ src/app/(pro)/professional_resume/ProjectCardSection.tsx
@@ -2,22 +2,28 @@
 
 import dynamic from "next/dynamic";
 
-// Toggle which implementation you want to show:
 const ProCardCSS = dynamic(() => import("./ProCardCSS"), { ssr: false });
 // const ScrollFlipCardScene = dynamic(() => import("./ScrollFlipCardScene"), { ssr: false });
 
 export default function ProjectCardSection() {
   return (
-    <>
-      {/* CSS 3D version (stable in dev) */}
-      <ProCardCSS />
+    <section className="relative w-full h-[100vh] flex items-center justify-center overflow-hidden">
+      {/* Rotated background block */}
+      <div className="absolute inset-0 z-0 transform rotate-45 bg-[#1a1a1a] rounded-[2rem] shadow-xl scale-125" />
 
-      {/* If you want to test WebGL later, swap the comments above/below: */}
-      {/* <section className="relative h-[160vh] !overflow-visible">
+      {/* Upright content on top */}
+      <div className="relative w-full h-[100vh] flex items-center justify-center overflow-hidden rotate-45">
+        <ProCardCSS />
+      </div>
+
+      {/* Optional WebGL version (commented out) */}
+      {/*
+      <section className="relative h-[160vh] !overflow-visible">
         <div className="sticky top-0 h-screen !overflow-visible">
           <ScrollFlipCardScene />
         </div>
-      </section> */}
-    </>
+      </section>
+      */}
+    </section>
   );
 }
